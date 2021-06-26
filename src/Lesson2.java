03bfc8a3ddc721cdfa9b0e78335b331ff9fde464
KungFury <proal92@gmail.com>
2021-06-26 16:02:56 +0300

    Исправленное задание 2

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,34 +1,41 @@
 public class Lesson2 {
     public static void main(String[] args) {
 
-        CompareSum(5,4);
-        CompareSum(5,5);
-        CompareSum(10,10);
-        CompareSum(10,11);
+        System.out.println( CompareSum(5,4));
+        System.out.println( CompareSum(5,5));
+        System.out.println(CompareSum(10,10));
+        System.out.println(CompareSum(10,11));
 
         AyeOrNay(-1);
         AyeOrNay(0);
         AyeOrNay(1);
 
-        Reverse(-1);
-        Reverse(0);
-        Reverse(1);
+        System.out.println(Reverse(-1));
+        System.out.println(Reverse(0));
+        System.out.println(Reverse(1));
 
         MonkeyBook("little monkey number ", 5);
 
-        LeapYear(1);
-        LeapYear(4);
-        LeapYear(100);
-        LeapYear(400);
-        LeapYear(800);
-        LeapYear(900);
+        System.out.println(LeapYear(1));
+        System.out.println(LeapYear(4));
+        System.out.println(LeapYear(100));
+        System.out.println(LeapYear(400));
+        System.out.println(LeapYear(800));
+        System.out.println(LeapYear(900));
+
+
+
+
+
+
 
 
     }
 
-    public static void CompareSum(int a, int b){
+    public static boolean CompareSum(int a, int b){
         boolean c=(a+b)>=10 && (a+b)<=20;
-        System.out.println(c);
+        return c;
+
     }
 
     public static void AyeOrNay(int a){
@@ -39,9 +46,8 @@ public class Lesson2 {
         System.out.println("Положительное");
     }
 
-    public static void Reverse(int a){
-        System.out.println(a<0);
-    }
+    public static boolean Reverse(int a){
+    return a<0;}
 
     public static void MonkeyBook(String str, int a){
         String s=str;
@@ -49,15 +55,15 @@ public class Lesson2 {
             System.out.println(s+i);
         }
     }
-    public static void LeapYear(int year){
+    public static boolean LeapYear(int year){
         if (year%400 == 0)
-            System.out.println("Високонсный");
+            return true;
         else if (year%100 == 0)
-            System.out.println("Не високонсный");
+            return false;
         else if (year%4 == 0)
-            System.out.println("Високонсный");
+            return true;
         else
-            System.out.println("Не високонсный");
+            return false;
     }
 
 
